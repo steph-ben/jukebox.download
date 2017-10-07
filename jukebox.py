5302c0d011e4843566ec17225056be5c4b2bf0bd
steph <steph@laptop.perso>
2017-10-07 23:24:27 +0200

    Add config file for user/password Add skipping files when already downloaded

--- jukebox.py
+++ jukebox.py
@@ -1,8 +1,12 @@
 #!/bin/env python
+"""
+Requirements:
+    - pip install python-requests
+    - pip install youtube-dl
+    - download ffmpeg from http://ffmpeg.zeranoe.com/builds/
+"""
 import os
 import requests
-import pprint as pp
-import q
 import youtube_dl
 
 
@@ -14,7 +18,14 @@ class JukeBoxApi(object):
         self.login()
         self.get_playlist()
         
-    def login(self, username="myuser", password="mypassword"):
+    def login(self, username=None, password=None):
+        if username is None:
+            print("Enter username: ")
+            username = raw_input().strip()
+        if password is None:
+            print("Enter password: ")
+            password = raw_input().strip()
+            
         print("Logging in to http://jukebox.today ...")
         req = self.s.post("http://jukebox.today/api/login_check", 
             {'_username': username, '_password': password})
@@ -28,13 +39,21 @@ class JukeBoxApi(object):
         
     def download_playlist(self, name="Daily", base_directory='.'):
         if name not in self.playlists:
-            print("Unknown playlist %s" % name)
+            print(u"Unknown playlist %s" % name)
             return
         directory_path = os.path.abspath(os.path.join(base_directory, name))
         
         playlist = self.playlists[name]
         for song in self.playlists[name]['songs']:
-            print("Downloading %s ..." % song['title'])
+            do_download = True
+            existing_file_list = os.listdir(u"%s" % directory_path)
+            for f in existing_file_list:
+                if (song['slug'] in f) and ("mp3" in f):
+                    do_download = False
+            if not do_download: 
+                print(u"Skipping", song['title'], song['slug'])
+                continue
+            print(u"Downloading ...", song['title'], song['slug'])
             self.download_url("http://youtu.be/%s" % song['slug'], directory_path=directory_path)
         
     def download_url(self, url, directory_path='.', filename=None):
